943f193ebf34f531180ebd5c66217acb8bf98a31
Angel Sharma <angel.dev.cse@gmail.com>
2024-08-18 14:52:16 +0600

    Added error handling and not found handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,10 +25,14 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`INSERT INTO invoices (customer_id, amount, status, date) VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+  try {
+    await sql`INSERT INTO invoices (customer_id, amount, status, date) VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
 
-  revalidatePath("/dashboard/invoices");
-  redirect("/dashboard/invoices");
+    revalidatePath("/dashboard/invoices");
+    redirect("/dashboard/invoices");
+  } catch (error) {
+    return { message: `Database error while creating invoice: ${error}` };
+  }
   // console.log(rawFormData);
 }
 
@@ -43,13 +47,24 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`UPDATE invoices SET customer_id = ${customerId}, amount = ${amount}, status=${status} where id= ${id}`;
+  try {
+    await sql`UPDATE invoices SET customer_id = ${customerId}, amount = ${amount}, status=${status} where id= ${id}`;
 
-  revalidatePath("/dashboard/invoices");
-  redirect("/dashboard/invoices");
+    revalidatePath("/dashboard/invoices");
+    redirect("/dashboard/invoices");
+  } catch (error) {
+    return { message: `Database error while updating invoice: ${error}` };
+  }
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id=${id}`;
-  revalidatePath("/dashboard/invoices");
+  throw new Error("Intentional error while deleting invoice");
+
+  try {
+    await sql`DELETE FROM invoices WHERE id=${id}`;
+    revalidatePath("/dashboard/invoices");
+    return { message: "Invoice deleted!" };
+  } catch (error) {
+    return { message: `Database error while deleting invoice: ${error}` };
+  }
 }
